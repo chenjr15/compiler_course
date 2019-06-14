e5cf354e6f5c7a5067e0374412bb4f5d9b7c2c5f
chenjr15 <chenjr15@live.cn>
2019-06-14 19:59:56 +0800

    将输出换成json

--- parser/src/parser.cc
+++ parser/src/parser.cc
@@ -2,6 +2,7 @@
 #include <vector>
 #include <cstdlib>
 #include <iostream>
+#include <fstream>
 using namespace std;
 void print_stack(vector<TokenStatus>&  stack) {
 
@@ -12,34 +13,44 @@ void print_stack(vector<TokenStatus>&  stack) {
 }
 
 
-void print_node(AstNode* t,int ident = 0) {
-
-
-    cout<<string(ident,' ');
-    cout<<t->value.to_string();
+string print_node(AstNode* t,int ident = 0) {
+    string s;
+    s += "{";
+    s+="\"name\":\"";
+    s+=Token::getTokenName(t->value.getTokenType());
+    s+="\",\"children\":";
     if (t->children.size()) {
-        cout<<"->";
-        print_node((t->children)[0],0);
-        for (size_t i = 1; i < t->children.size(); i++)
-        {
-            print_node((t->children)[i],ident+t->value.to_string().size()+2);
+        s+="[";
+        for (size_t i = 0; i < t->children.size(); i++) {
+            s+=  print_node((t->children)[i],ident+t->value.to_string().size()+2);
+
         }
-        // cout<<endl;
-    }else
-    {
-    cout<<endl;
-        
+        s += "]";
+
+    } else {
+        s+="None";
     }
+    s += "},";
     
-
-    
-
+    return s;
 }
-void print_tree(vector<AstNode*> t){
-    for(auto node : t)
-    {
-        print_node(node);
+void print_tree(vector<AstNode*> t) {
+    string s;
+    s += "[";
+    for(auto node : t) {
+        s+=print_node(node);
     }
+    s += "]";
+
+    // cout<<s;
+    // cout<<endl;
+    // 调用Python输出树
+    ofstream f("t.py",ios::trunc);
+    f<<"import json\ns = "<<s<<"\ns = json.dumps(s,indent=2)\nprint(s)\n";
+    f.close();
+    system("python3 t.py");
+    cout<<endl;
+
 }
 AstNode* parse(list<Token> &tokens) {
     vector<AstNode*> tree_stack;
@@ -51,7 +62,6 @@ AstNode* parse(list<Token> &tokens) {
     while (true) {
         cout<<"\n\n----------"<<endl;
         print_stack(stack);
-        print_tree(tree_stack);
         if (token_iter != tokens.end()) {
             cur_token = *token_iter;
         } else {
@@ -89,11 +99,10 @@ AstNode* parse(list<Token> &tokens) {
             }
             // 剪枝
             AstNode* new_node = new AstNode(prod.left);
-            for (int i = right.size() - 1; i >= 0; i--)
-            {
+            for (int i = right.size() - 1; i >= 0; i--) {
                 new_node->add_child(right[i]);
             }
-            
+
             tree_stack.push_back(new_node);
             // 根据goto表寻找当前规约后的新状态
             int status = (--stack.end())->second;
@@ -102,6 +111,8 @@ AstNode* parse(list<Token> &tokens) {
             stack.push_back({prod.left,status});
         } else if (action=="accept") {
             // 成功
+            print_tree(tree_stack);
+
             break;
         }
 
